42cccc4e214eef549188d6a03173a159e75e186b
SverreSB <sverre.sb@gmail.com>
2019-01-05 10:21:16 -0800

    Started new directory 'promises' to work with promises'

--- playing_around/index.js
+++ playing_around/index.js
@@ -4,25 +4,26 @@ console.log("Before timeout...");
 getUser(1, (user) => {
     console.log('User', user);
     //Getting files after user is retreived
-    getFiles(user.name, (files) => {
+    getFiles(user, (files) => {
         console.log(files);
     });
+    //I am finishing this directory here. I will start working in new directory on promises from here
 });
 
 console.log("After timeout...")
 
 //Using a callback to retreive user after timeout, to simulate when retriveing data from DB 
 function getUser(id, callback){
+    console.log("Reading from database...");
     setTimeout(() => {
-        console.log("Reading from database...");
         callback({id: id, name: "Test"});
     }, 2000); 
 }
 
 //For reaturning files if user id === 1
-function getFiles(username, callback){
+function getFiles(user, callback){
+    console.log("Finding files...");
     setTimeout(() => {
-        console.log("Reasing files...")
         callback(['javaDoc.pdf', "learningToCode.pdf", "README.md"]);
     }, 3000);
 }
