ddd50a36244508c825da1b216549b319166de4ee
SverreSB <sverre.sb@gmail.com>
2019-01-05 08:58:36 -0800

    put the timeout inside a function to see behavior of program

--- playing_around/index.js
+++ playing_around/index.js
@@ -1,8 +1,17 @@
 console.log("Before timeout...");
 
-//This will print after "After timeout...", because it has a timeout on two seconds
-setTimeout(() => {
-    console.log("Reading from database...");
-}, 2000);
+//getting a user object with function getUser, which is supposed to simulate getting data from DB
+const user = getUser(1);
+//Will log "undefined" since there is a timeout in function getUser
+console.log(user);
+console.log("After timeout...")
 
-console.log("After timeout...");
+function getUser(id){
+    setTimeout(() => {
+        console.log("Reading from database...");
+        return {id: id, name: ""};
+    }, 2000); 
+}
+
+
+//console.log("After timeout...");
