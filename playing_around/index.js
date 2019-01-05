54ad32706c61f6434cef78f2bda72c4c70543756
SverreSB <sverre.sb@gmail.com>
2019-01-05 09:38:07 -0800

    added function to return files user 1 has

--- playing_around/index.js
+++ playing_around/index.js
@@ -3,6 +3,10 @@ console.log("Before timeout...");
 
 getUser(1, (user) => {
     console.log('User', user);
+    //Getting files after user is retreived
+    getFiles(user, (files) => {
+        console.log(files);
+    });
 });
 
 console.log("After timeout...")
@@ -13,6 +17,13 @@ function getUser(id, callback){
         console.log("Reading from database...");
         callback({id: id, name: "Test"});
     }, 2000); 
+}
+
+//For reaturning files if user id === 1
+function getFiles(user, callback){
+    if(user.id === 1){
+        callback(['javaDoc.pdf', "learningToCode.pdf", "README.md"]);
+    }
 }
 
 
