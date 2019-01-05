fd16dbf07567649c1a28c983fcecc26dceed68fb
SverreSB <sverre.sb@gmail.com>
2019-01-05 14:49:26 -0800

    Consume promises and create them using getUser and getFiles

--- promises/index.js
+++ promises/index.js
@@ -1,5 +1,31 @@
-//Creating promise
-const p = new Promise( (resolve, reject) => {
+//consume promises
+getUser(1)
+    .then(user => getFiles(user.name))
+    .then(files => console.log(files[0]));
+
+
+// Creating promises in the functions
+
+function getUser(id){
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            console.log("Reading user data...")
+            resolve({id: id, name: "New User"});
+        }, 2000); 
+    });   
+}
+
+function getFiles(username){
+    return new Promise( (resolve, reject) => { 
+        setTimeout(() => {
+            console.log("Finding files...")
+            resolve(['javaDoc.pdf', "learningToCode.pdf", "README.md"]);
+        }, 2000);
+    });   
+}
+
+//Create promise
+/*const p = new Promise( (resolve, reject) => {
     setTimeout( () => {
         //resolve("Test");
         reject(new Error('Something went wrong'));
@@ -9,4 +35,4 @@ const p = new Promise( (resolve, reject) => {
 //Consume promise
 p.
     then(res => console.log('Result', res))
-    .catch(err => console.log('Error', err.message));
+    .catch(err => console.log('Error', err.message));*/
