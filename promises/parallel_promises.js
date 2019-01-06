fae0f155f7fb37f3f2f288d20411e1061ab788c7
SverreSB <sverre.sb@gmail.com>
2019-01-05 16:13:02 -0800

    Added catch and using reject in promis p2

--- promises/parallel_promises.js
+++ promises/parallel_promises.js
@@ -7,13 +7,15 @@ const p1 = new Promise( (resolve) => {
     }, 2000); 
 });
 
-const p2 = new Promise( (resolve) => {
+const p2 = new Promise( (resolve, reject) => {
     setTimeout( () => {
         console.log('Second promise ...');
-        resolve({id : 2, user : "Bob"});
+        //resolve({id : 2, user : "Bob"});
+        reject(new Error('Error in promise 2'));
     }, 2000);
 });
 
 //Both of the promises are resolved
 Promise.all([p1, p2])
     .then(result => console.log(result))
+    .catch(err => console.log('Error - ', err.message));
