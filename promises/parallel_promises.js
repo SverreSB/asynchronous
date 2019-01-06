bc666b333c2396c4cf8422376e0c2370010cec41
SverreSB <sverre.sb@gmail.com>
2019-01-05 16:02:32 -0800

    Added second promise

--- promises/parallel_promises.js
+++ promises/parallel_promises.js
@@ -1,11 +1,19 @@
 //Running two parallel promises
 
-const p1 = new Promise((resolve) => {
+const p1 = new Promise( (resolve) => {
     setTimeout( () => {
-        console.log('First promise ... ');
+        console.log('First promise ...');
         resolve({id : 1, user : "Alice"});
-    }); 
+    }, 2000); 
 });
 
-Promise.all([p1])
-    .then(result => console.log(result));
+const p2 = new Promise( (resolve) => {
+    setTimeout( () => {
+        console.log('Second promise ...');
+        resolve({id : 2, user : "Bob"});
+    }, 2000);
+});
+
+//Both of the promises are resolved
+Promise.all([p1, p2])
+    .then(result => console.log(result))
