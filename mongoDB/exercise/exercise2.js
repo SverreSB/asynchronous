9a241cb9806f478c435ebbbc078cc2037ede5583
SverreSB <sverre.sb@gmail.com>
2019-01-07 07:46:48 -0800

    Created new exercise file, criteria as header. Also fixed exercise2,js' style

--- mongoDB/exercise/exercise2.js
+++ mongoDB/exercise/exercise2.js
@@ -8,27 +8,27 @@
  const mongoose = require('mongoose');
 
  mongoose.connect('mongodb://localhost/mongo-exercise1')
- .then(() => console.log('Connected to mongo-exercise1..'))
- .catch(err => console.error('Could not connect to mongoDB, mongo-exercise1', err));
+    .then(() => console.log('Connected to mongo-exercise1..'))
+    .catch(err => console.error('Could not connect to mongoDB, mongo-exercise1', err));
 
 const courseSchema = new mongoose.Schema({
- tags: [String],
- date: {type: Date, default: Date.now},
- name: String,
- author: String,
- isPublished: Boolean
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    name: String,
+    author: String,
+    isPublished: Boolean
 });
 
 const Course = mongoose.model('Course', courseSchema);
 
 //Async function to retrive data from database. 
 //Will console.log front- and backend courses that are published and price in desc order
- async function getCourses(){
+async function getCourses(){
     const courses = await Course
     .find( { isPublished: true, tags: { $in: ['backend', 'frontend'] } } )
     .sort({price: -1})
     .select({name: 1, author: 1});
     console.log(courses);
- }
+}
 
- getCourses();
+getCourses();
